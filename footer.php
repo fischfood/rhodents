fc8345d8d74b3ab0abfbe7309d6deb7e24e61cb4
Brian Fischer <brian@linchpin.com>
2024-05-29 22:46:10 -0400

    Initial Commit

---
+++ footer.php
@@ -0,0 +1,15 @@
+    </div>
+
+    <footer id="footer">
+        <div class="skyline">
+            <img src="<?php echo url('images/skyline.png'); ?>" />
+            <img class="skyline-wheel"src="<?php echo url('images/skyline-wheel.png'); ?>" />
+        </div>
+        <div class="footer-content">
+            <div class="container">
+                <p>Rhodents <span class="copy">&copy;</span> <?php echo date('Y'); ?>. All Rights Reserved.</p>
+            </div>
+        </div>
+    </footer>
+
+</body>

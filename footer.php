1f9d41b294377e9de5b609dce69005d2a2871553
Brian Fischer <contact@brianfischer.me>
2024-05-30 03:04:31 -0400

    Footer Link to BF.me

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
         </div>
         <div class="footer-content">
             <div class="container">
-                <p>Rhodents <span class="copy">&copy;</span> <?php echo date('Y'); ?>. All Rights Reserved.</p>
+                <p>Rhodents <span class="copy">&copy;</span> <?php echo date('Y'); ?>. All Rights Reserved. Built by <a class="bf" href="https://brianfischer.me">Brian Fischer</a></p>
             </div>
         </div>
     </footer>
